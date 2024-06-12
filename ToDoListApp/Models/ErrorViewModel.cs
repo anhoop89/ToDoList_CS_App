b573293805184abf959890f51f7c6196f9b5ee3a
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-12 00:12:35 -0700

    checking

--- ToDoListApp/Models/ErrorViewModel.cs
+++ ToDoListApp/Models/ErrorViewModel.cs
@@ -6,4 +6,12 @@ namespace ToDoListApp.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 }
